4e7382dd4ad5251e374431aaafc26daa402057c9
lzwjava <lzwjava@gmail.com>
2016-01-31 02:29:25 +0800

    一血！

---
+++ WeImg/FirstViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FirstViewController.swift
+//  WeImg
+//
+//  Created by lzw on 16/1/28.
+//  Copyright © 2016年 WeImg Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
