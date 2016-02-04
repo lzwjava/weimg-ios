1f63baf76642e76ddc8a7af2fa48f1a20df3b8dd
lzwjava <lzwjava@gmail.com>
2016-02-04 18:01:40 +0800

    增加部分xib和swift

--- WeImg/Helpers/YepConfig.swift
+++ WeImg/Helpers/YepConfig.swift
@@ -9,6 +9,8 @@
 import Foundation
 import UIKit
 
+let avatarFadeTransitionDuration: NSTimeInterval = 0.0
+
 class YepConfig {
     
     class func avatarCompressionQuality() -> CGFloat {
@@ -21,5 +23,33 @@ class YepConfig {
     
     class func avatarMaxSize() -> CGSize {
         return CGSize(width: 414, height: 414)
+    }
+    
+    struct Settings {
+        static let userCellAvatarSize: CGFloat = 80
+        
+        static let introFont: UIFont = {
+            if #available(iOS 8.2, *) {
+                return UIFont.systemFontOfSize(12, weight: UIFontWeightLight)
+            } else {
+                return UIFont(name: "HelveticaNeue-Light", size: 12)!
+            }
+        }()
+        
+        static let introInset: CGFloat = 20 + userCellAvatarSize + 20 + 10 + 11 + 20
+    }
+    
+    
+    struct EditProfile {
+        
+        static let introFont: UIFont = {
+            if #available(iOS 8.2, *) {
+                return UIFont.systemFontOfSize(15, weight: UIFontWeightLight)
+            } else {
+                return UIFont(name: "HelveticaNeue-Light", size: 15)!
+            }
+        }()
+        
+        static let introInset: CGFloat = 20 + 20
     }
 }
